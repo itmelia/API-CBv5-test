ff78eca980c239b71fb2bdbd6122f29235b253a0
elmiramammadli <eliaamammadli@gmail.com>
2023-09-06 19:38:17 -0700

    added env vars

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,13 +1,14 @@
 import { expect } from 'chai'
 import request from 'supertest'
+import 'dotenv/config'
 
 describe('Authentication', function () {
   describe('Authentication with valid credentials', function () {
     it('Validate status code', async function () {
       let res
-      res = await request('https://clientbase-server.herokuapp.com/v5')
+      res = await request(process.env.BASE_URL)
         .post('/user/login')
-        .send({ email: 'elmiller@gmail.com', password: 'qwerty123' })
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
 
       //console.log(res)
 
@@ -16,9 +17,9 @@ describe('Authentication', function () {
 
     it('Validate response message', async function () {
       let res
-      res = await request('https://clientbase-server.herokuapp.com/v5')
+      res = await request(process.env.BASE_URL)
         .post('/user/login')
-        .send({ email: 'elmiller@gmail.com', password: 'qwerty123' })
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
 
       //console.log(res)
 
@@ -27,9 +28,9 @@ describe('Authentication', function () {
 
     it('Check the token exist', async function () {
       let res
-      res = await request('https://clientbase-server.herokuapp.com/v5')
+      res = await request(process.env.BASE_URL)
         .post('/user/login')
-        .send({ email: 'elmiller@gmail.com', password: 'qwerty123' })
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
 
       //console.log(res)
 
@@ -38,27 +39,27 @@ describe('Authentication', function () {
 
     it('Check the role', async () => {
       let res
-      res = await request('https://clientbase-server.herokuapp.com/v5')
+      res = await request(process.env.BASE_URL)
         .post('/user/login')
-        .send({ email: 'elmiller@gmail.com', password: 'qwerty123' })
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
 
       expect(res.body.payload.user.roles).to.be.an('array')
     })
 
     it('Check the creation time', async () => {
       let res
-      res = await request('https://clientbase-server.herokuapp.com/v5')
+      res = await request(process.env.BASE_URL)
         .post('/user/login')
-        .send({ email: 'elmiller@gmail.com', password: 'qwerty123' })
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
 
       expect(res.body.payload.user.createdAt).to.be.a('string')
     })
 
     it('Check acl', async () => {
       let res
-      res = await request('https://clientbase-server.herokuapp.com/v5')
+      res = await request(process.env.BASE_URL)
         .post('/user/login')
-        .send({ email: 'elmiller@gmail.com', password: 'qwerty123' })
+        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
 
       expect(res.body.payload.acl).to.be.an('array')
     })
@@ -67,9 +68,9 @@ describe('Authentication', function () {
   describe('Authentication with not valid credentials', () => {
     it('Check the status code', async () => {
       let res
-      res = await request('https://clientbase-server.herokuapp.com/v5')
+      res = await request(process.env.BASE_URL)
         .post('/user/login')
-        .send({ email: 'Eelmiller@gmail.com', password: 'qwerty123' })
+        .send({ email: 'elkmfkdms@gmail.com', password: process.env.PASSWORD })
 
       expect(res.statusCode).to.eq(400)
     })
