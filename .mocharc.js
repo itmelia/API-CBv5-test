dcdf5f3be6eb01cd078bf95d3ab5139d44a8b83e
elmiramammadli <eliaamammadli@gmail.com>
2023-10-04 21:39:14 -0700

    update mochars

--- .mocharc.js
+++ .mocharc.js
@@ -4,4 +4,10 @@ module.exports = {
   spec: 'specs/**/*.js',
   exclude: 'specs/example*', //  '*/example*'
   file: 'project-config/auth-global-hook.js',
+  reporter: 'mochawesome',
+  reporterOption: [
+    'json=false',
+    'reportDir=MyReports',
+    'reportFileName=UpdatedReport',
+  ],
 }
